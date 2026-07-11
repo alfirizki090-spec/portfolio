abf10df31319828ef8f18a1f3e62e60676452e6e
Rizki Alfi <alfirizki090@gmail.com>
2026-07-12 06:00:29 +0700

    chore: setup initial portfolio structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,132 @@
-import Image from "next/image";
+const navLink = [
+  { name: "ABOUT", url: "#about" },
+  { name: "SKILLS", url: "#skills" },
+  { name: "PROJECTS", url: "#projects" },
+];
+
+const contactLink = [
+  { name: "Github", url: "" },
+  { name: "LinkedIn", url: "" },
+  { name: "Email", url: "" },
+];
+
+const techStack = [
+  { name: "React", icon: "" },
+  { name: "Next.js", icon: "" },
+  { name: "TypeScript", icon: "" },
+  { name: "Tailwind CSS", icon: "" },
+  { name: "Git", icon: "" },
+  { name: "shadcn/ui", icon: "" },
+];
+
+const project = [
+  { name: "project 1", url: "" },
+  { name: "project 2", url: "" },
+];
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="flex">
+      <header className="lg:w-1/2">
+
+      {/* PROFILE */}
+        <h1>Rizki Alfi</h1>
+        <h2>Frontend Developer</h2>
+        <p>I build clean, performant, and accessible web interfasces</p>
+        <br />
+
+        {/* NAVIGATION */}
+        <nav>
+          <ul>
+            {navLink.map((item) => (
+              <li key={item.name}>
+                <a href="item.url">
+                  <span>{item.name}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <br />
+
+        {/* CONTACTS */}
+        <nav>
+          <ul>
+            {contactLink.map((item) => (
+              <li key={item.name}>
+                <a href="item.url">
+                  <span>{item.name}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <div className="lg:w-1/2">
+        <main>
+
+          {/* SECTION 1: ABOUT */}
+          <section id="about">
+            <p>
+              Hi there! I'm Alfi, and I build modern digital experiences. My
+              journey with code started back in vocational high school (SMK)
+              where I studied Software Engineering. Today, I am fully focused on
+              frontend development, specializing in crafting clean, accessible,
+              and detail-oriented user interfaces.
+            </p> <br />
+            <p>
+              Before diving deep into engineering, I helped build and run
+              operations for a family-owned garment business. I wore many
+              hats—from managing HR payroll data and logistics to hands-on
+              packing and product deliveries. Experiencing the chaotic,
+              fast-paced nature of a scaling business taught me extreme
+              adaptability, resilience, and a sharp eye for operational
+              efficiency—traits I now apply to writing robust, scalable code.
+            </p> <br />
+            <p>
+              My path to tech is driven by pure grit. Currently, I fund my
+              transition by working as a ride-hailing driver during the day,
+              which allows me to dedicate 6 hours every single night to
+              mastering frontend architecture and professional English. This
+              intense daily discipline is fueled by my ultimate goal: securing a
+              remote role in the global tech industry.
+            </p> <br />
+            <p>
+              When I step away from the keyboard, I enjoy analyzing
+              cryptocurrency market trends, studying the Japanese language, and
+              simply taking a moment to breathe before the next night-shift
+              coding session begins.
+            </p> <br />
+          </section> <br />
+
+          {/* SECTION 2: TECH STACKS */}
+          <section id="skills">
+            <ul className="flex gap-2">
+              {techStack.map((item) => (
+                <li key={item.name}>{item.name}</li>
+              ))}
+            </ul>
+          </section> <br />
+
+          {/* SECTION 3: PROJECTS */}
+          <section id="projects">
+            <nav>
+              <ul>
+                {project.map((item) => (
+                  <li key={item.name}>{item.name}</li>
+                ))}
+              </ul>
+            </nav>
+          </section>
+        </main> <br />
+
+        <footer>
+          <p>
+            @ 2026 Rizki Alfi. <br />
+            Built with Next.js, TypeScript, Tailwind CSS & shadcn/ui.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        </footer>
+      </div>
     </div>
   );
 }
